371ee0efe81a0175d3ef45e77b8b995998689cf2
Jason Kraus <jasonk@cukerinteractive.com>
2012-10-03 14:53:54 -0700

    factor dotpath to its own resource

--- dockitresource/hyperobjects.py
+++ dockitresource/hyperobjects.py
@@ -2,22 +2,12 @@ from hyperadmin.hyperobjects import State, Namespace
 
 class DotpathState(State):
     def set_dotpath(self, val):
-        self.params['_dopath'] = val
+        self['dopath'] = val
     
     def get_dotpath(self):
-        return self.params.get('_dotpath', '')
+        return self.get('dotpath', '')
     
     dotpath = property(get_dotpath, set_dotpath)
-    
-    def get_query_string(self, new_params=None, remove=None):
-        if new_params and 'dotpath' in new_params:
-            val = new_params.pop('dotpath')
-            new_params['_dotpath'] = val
-        if remove and 'dotpath' in remove:
-            remove = list(remove)
-            remove.remove('dotpath')
-            remove.append('_dotpath')
-        return super(DotpathState, self).get_query_string(new_params=new_params, remove=remove)
 
 class DotpathNamespace(Namespace):
     pass
--- dockitresource/resources.py
+++ dockitresource/resources.py
@@ -10,33 +10,9 @@ from dockitresource import views
 from dockitresource.changelist import DocumentChangeList
 from dockitresource.hyperobjects import DotpathState, DotpathNamespace
 
-class BaseDocumentResource(CRUDResource):
+class DocumentResourceMixin(object):
     state_class = DotpathState
-    #TODO support the following:
-    #raw_id_fields = ()
-    fields = None
-    exclude = []
-    #fieldsets = None
-    #filter_vertical = ()
-    #filter_horizontal = ()
-    #radio_fields = {}
-    #prepopulated_fields = {}
-    formfield_overrides = {}
-    #readonly_fields = ()
-    #declared_fieldsets = None
-    
-    #save_as = False
-    #save_on_top = False
-    changelist_class = DocumentChangeList
-    
-    #list display options
-    list_per_page = 100
-    list_max_show_all = 200
-    
-    list_view = views.DocumentListView
-    add_view = views.DocumentCreateView
-    detail_view = views.DocumentDetailView
-    delete_view = views.DocumentDeleteView
+    dotpath_resource = None
     
     @property
     def document(self):
@@ -45,6 +21,144 @@ class BaseDocumentResource(CRUDResource):
     @property
     def opts(self):
         return self.document._meta
+    
+    def get_app_name(self):
+        return self.opts.app_label
+    app_name = property(get_app_name)
+    
+    def get_resource_name(self):
+        return self.opts.module_name
+    resource_name = property(get_resource_name)
+    
+    def get_prompt(self):
+        return self.resource_name
+    
+    def get_view_kwargs(self):
+        kwargs = super(DocumentResourceMixin, self).get_view_kwargs()
+        kwargs['document'] = self.document
+        return kwargs
+    
+    def _get_schema_fields(self):
+        for field in self.schema._meta.fields.itervalues():
+            if getattr(field, 'schema', None):
+                yield (field, field.schema, False)
+            elif getattr(field, 'subfield', None) and getattr(field.subfield, 'schema', None):
+                yield (field, field.subfield.schema, True)
+    
+    def _get_static_schema_fields(self):
+        fields = list()
+        for field, schema, many in self._get_schema_fields():
+            if not schema._meta.is_dynamic():
+                fields.append((field, schema, many))
+        return fields
+    
+    def _get_complex_fields(self):
+        from dockit import schema
+        #list fields of "primitives" are not considered complex enough, but if it embeds a schema it is complex
+        for field in self.schema._meta.fields.itervalues():
+            if isinstance(field, schema.SchemaField) or isinstance(field, schema.GenericSchemaField):
+                yield field
+            elif isinstance(field, schema.ListField):
+                if getattr(field.subfield, 'schema', None) or isinstance(field.subfield, schema.GenericSchemaField):
+                    yield field
+    
+    def get_html_type_from_field(self, field):
+        from dockit.forms import widgets
+        widget = field.field.widget
+        if isinstance(widget, widgets.PrimitiveListWidget):
+            return 'list'
+        return super(DocumentResourceMixin, self).get_html_type_from_field(field)
+    
+    def get_namespaces(self):
+        namespaces = super(DocumentResourceMixin, self).get_namespaces()
+        if self.dotpath_resource and self.state.item is not None and self.state.get('view_class', None) == 'change_form':
+            item = self.state.item
+            
+            for field in self._get_complex_fields():
+                name = 'inline-%s' % field.name
+                if self.state.dotpath:
+                    dotpath = self.state.dotpath+'.'+field.name
+                else:
+                    dotpath = field.name
+                inline = self.dotpath_resource.fork_state(dotpath=dotpath, item=item)
+                print dotpath, inline.get_item_url(item)
+                link = inline.get_item_link(item, url=inline.get_item_url(item))
+                namespace = DotpathNamespace(name=name, link=link, state=inline.state)
+                namespaces[name] = namespace
+            
+            print namespaces
+        return namespaces
+    
+    def get_item_namespaces(self, item):
+        namespaces = super(DocumentResourceMixin, self).get_item_namespaces(item)
+        print namespaces
+        
+        for field in self._get_complex_fields():
+            name = 'inline-%s' % field.name
+            inline = self.fork_state()
+            if self.state.dotpath:
+                dotpath = self.state.dotpath+'.'+field.name
+            else:
+                dotpath = field.name
+            inline.state.dotpath =  dotpath #our state class recognizes this variable
+            link = inline.get_item_link(item, url=inline.get_item_url(item)+inline.state.get_query_string())
+            namespace = DotpathNamespace(name=name, link=link, state=inline.state)
+            namespaces[name] = namespace
+        print namespaces
+        return namespaces
+    
+    def get_queryset(self, user):
+        queryset = self.resource_adaptor.objects.all()
+        if not self.has_change_permission(user):
+            queryset = queryset.none()
+        return queryset
+
+class DotpathResource(DocumentResourceMixin, CRUDResource):
+    changelist_class = DocumentChangeList
+    
+    list_view = views.DocumentListView
+    add_view = views.DocumentCreateView
+    detail_view = views.DocumentDetailView #TODO this needs to double as a list view
+    delete_view = views.DocumentDeleteView
+    
+    def get_base_url_name(self):
+        return '%s%s_' % (self.parent.get_base_url_name(), 'dotpath')
+    
+    def get_urls(self):
+        def wrap(view, cacheable=False):
+            return self.as_view(view, cacheable)
+        
+        init = self.get_view_kwargs()
+        
+        # Admin-site-wide views.
+        base_name = self.get_base_url_name()
+        urlpatterns = self.get_extra_urls()
+        urlpatterns += patterns('',
+            url(r'^$',
+                wrap(self.detail_view.as_view(**init)),
+                name='%sdetail' % base_name),
+            url(r'^add/$',
+                wrap(self.add_view.as_view(**init)),
+                name='%sadd' % base_name),
+            url(r'^delete/$',
+                wrap(self.delete_view.as_view(**init)),
+                name='%sdelete' % base_name),
+        )
+        return urlpatterns
+    
+    def get_add_url(self):
+        return self.reverse('%sadd' % self.get_base_url_name(), dotpath=self.state.dotpath)
+    
+    def get_delete_url(self, item):
+        return self.reverse('%sdelete' % self.get_base_url_name(), pk=item.instance.pk, dotpath=self.state.dotpath)
+    
+    def get_item_url(self, item):
+        return self.reverse('%sdetail' % self.get_base_url_name(), pk=item.instance.pk, dotpath=self.state.dotpath)
+    
+    def get_absolute_url(self):
+        if self.state.item:
+            return self.get_item_url(self.state.item)
+        return None
     
     def get_field(self, schema, dotpath):
         field = None
@@ -102,63 +216,8 @@ class BaseDocumentResource(CRUDResource):
         assert issubclass(schema, Schema)
         return schema
     
-    def get_app_name(self):
-        return self.opts.app_label
-    app_name = property(get_app_name)
-    
-    def get_resource_name(self):
-        return self.opts.module_name
-    resource_name = property(get_resource_name)
-    
-    def get_prompt(self):
-        return self.resource_name
-    
-    def get_view_kwargs(self):
-        kwargs = super(BaseDocumentResource, self).get_view_kwargs()
-        kwargs['document'] = self.document
-        return kwargs
-    
-    def get_active_index(self, **kwargs):
-        return self.get_queryset(user=self.state['auth'])
-    
-    def lookup_allowed(self, lookup, value):
-        return True #TODO
-    
-    def get_queryset(self, user):
-        queryset = self.resource_adaptor.objects.all()
-        if not self.has_change_permission(user):
-            queryset = queryset.none()
-        return queryset
-    
-    def _get_schema_fields(self):
-        for field in self.schema._meta.fields.itervalues():
-            if getattr(field, 'schema', None):
-                yield (field, field.schema, False)
-            elif getattr(field, 'subfield', None) and getattr(field.subfield, 'schema', None):
-                yield (field, field.subfield.schema, True)
-    
-    def _get_static_schema_fields(self):
-        fields = list()
-        for field, schema, many in self._get_schema_fields():
-            if not schema._meta.is_dynamic():
-                fields.append((field, schema, many))
-        return fields
-    
-    def _get_complex_fields(self):
-        from dockit import schema
-        #list fields of "primitives" are not considered complex enough, but if it embeds a schema it is complex
-        for field in self.schema._meta.fields.itervalues():
-            if isinstance(field, schema.SchemaField) or isinstance(field, schema.GenericSchemaField):
-                yield field
-            elif isinstance(field, schema.ListField):
-                if getattr(field.subfield, 'schema', None) or isinstance(field.subfield, schema.GenericSchemaField):
-                    yield field
-    
     def get_excludes(self):
-        if self.state.dotpath:
-            excludes = set()
-        else:
-            excludes = set(self.exclude)
+        excludes = set()
         for field in self._get_complex_fields():
             excludes.add(field.name)
         return list(excludes)
@@ -176,54 +235,80 @@ class BaseDocumentResource(CRUDResource):
                 #TODO formfield overides
                 #TODO fields
         return AdminForm
+
+class BaseDocumentResource(DocumentResourceMixin, CRUDResource):
+    #TODO support the following:
+    #raw_id_fields = ()
+    fields = None
+    exclude = []
+    #fieldsets = None
+    #filter_vertical = ()
+    #filter_horizontal = ()
+    #radio_fields = {}
+    #prepopulated_fields = {}
+    formfield_overrides = {}
+    #readonly_fields = ()
+    #declared_fieldsets = None
     
-    def get_html_type_from_field(self, field):
-        from dockit.forms import widgets
-        widget = field.field.widget
-        if isinstance(widget, widgets.PrimitiveListWidget):
-            return 'list'
-        return super(BaseDocumentResource, self).get_html_type_from_field(field)
+    #save_as = False
+    #save_on_top = False
+    changelist_class = DocumentChangeList
+    dotpath_resource_class = DotpathResource
     
-    def get_namespaces(self):
-        namespaces = super(BaseDocumentResource, self).get_namespaces()
-        if self.state.item is not None and self.state.get('view_class', None) == 'change_form':
-            item = self.state.item
-            
-            for field in self._get_complex_fields():
-                name = 'inline-%s' % field.name
-                inline = self.fork_state()
-                if self.state.dotpath:
-                    dotpath = self.state.dotpath+'.'+field.name
-                else:
-                    dotpath = field.name
-                print dotpath
-                inline.state.dotpath =  dotpath #our state class recognizes this variable
-                link = inline.get_item_link(item, url=inline.get_item_url(item)+inline.state.get_query_string())
-                namespace = DotpathNamespace(name=name, link=link, state=inline.state)
-                namespaces[name] = namespace
-            
-            print namespaces
-        return namespaces
+    #list display options
+    list_per_page = 100
+    list_max_show_all = 200
     
-    def get_item_namespaces(self, item):
-        namespaces = super(BaseDocumentResource, self).get_item_namespaces(item)
-        print namespaces
-        
+    list_view = views.DocumentListView
+    add_view = views.DocumentCreateView
+    detail_view = views.DocumentDetailView
+    delete_view = views.DocumentDeleteView
+    
+    def __init__(self, *args, **kwargs):
+        super(BaseDocumentResource, self).__init__(*args, **kwargs)
+        self.dotpath_resource = self.create_dotpath_resource()
+    
+    def create_dotpath_resource(self):
+        cls = self.get_dotpath_resource_class()
+        return cls(resource_adaptor=self.resource_adaptor, site=self.site, parent_resource=self)
+    
+    def get_dotpath_resource_class(self):
+        return self.dotpath_resource_class
+    
+    @property
+    def schema(self):
+        return self.document
+    
+    def get_extra_urls(self):
+        urlpatterns = super(BaseDocumentResource, self).get_extra_urls()
+        urlpatterns += patterns('',
+            url(r'^(?P<pk>\w+)/(?P<dotpath>\w+)/',
+                include(self.dotpath_resource.urls)),
+        )
+        return urlpatterns
+    
+    def get_active_index(self, **kwargs):
+        return self.get_queryset(user=self.state['auth'])
+    
+    def lookup_allowed(self, lookup, value):
+        return True #TODO
+    
+    def get_excludes(self):
+        excludes = set(self.exclude)
         for field in self._get_complex_fields():
-            name = 'inline-%s' % field.name
-            inline = self.fork_state()
-            if self.state.dotpath:
-                dotpath = self.state.dotpath+'.'+field.name
-            else:
-                dotpath = field.name
-            inline.state.dotpath =  dotpath #our state class recognizes this variable
-            link = inline.get_item_link(item, url=inline.get_item_url(item)+inline.state.get_query_string())
-            namespace = DotpathNamespace(name=name, link=link, state=inline.state)
-            namespaces[name] = namespace
-        print namespaces
-        return namespaces
+            excludes.add(field.name)
+        return list(excludes)
     
-    #TODO delete with dotpaths
+    def get_form_class(self):
+        if self.form_class:
+            return self.form_class
+        class AdminForm(forms.DocumentForm):
+            class Meta:
+                document = self.document
+                exclude = self.get_excludes()
+                #TODO formfield overides
+                #TODO fields
+        return AdminForm
 
 class TemporaryDocumentResource(BaseDocumentResource):
     '''
@@ -262,7 +347,7 @@ class TemporaryDocumentResource(BaseDocumentResource):
         return queryset
     
     def get_base_url_name(self):
-        return '%s_%s_%s_' % (self.parent.app_name, self.parent.resource_name, 'tempdoc')
+        return '%s%s_' % (self.parent.get_base_url_name(), 'tempdoc')
     
     #TODO create may make a copy of an existing document or start a new one
     def get_copy_url(self):
@@ -322,7 +407,7 @@ class TemporaryDocumentResource(BaseDocumentResource):
         return link.clone(form=form)
 
 class DocumentResource(BaseDocumentResource):
-    temporary_document_resoure_class = TemporaryDocumentResource
+    temporary_document_resource_class = TemporaryDocumentResource
     
     def __init__(self, *args, **kwargs):
         super(DocumentResource, self).__init__(*args, **kwargs)
@@ -333,7 +418,7 @@ class DocumentResource(BaseDocumentResource):
         return cls(resource_adaptor=self.resource_adaptor, site=self.site, parent_resource=self)
     
     def get_temporary_document_resource_class(self):
-        return self.temporary_document_resoure_class
+        return self.temporary_document_resource_class
     
     def get_extra_urls(self):
         urlpatterns = super(DocumentResource, self).get_extra_urls()
