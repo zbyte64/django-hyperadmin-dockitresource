d3da4b61667cdb2cc43feede43a2a20ff920fb51
Jason Kraus <jasonk@cukerinteractive.com>
2012-11-05 15:24:52 -0800

    support new index links

--- dockitresource/changelist.py
+++ dockitresource/changelist.py
@@ -1,46 +1,32 @@
 from hyperadmin.resources.crud.changelist import ChangeList, FilterSection
 
 class DocumentChangeList(ChangeList):
-    def __init__(self, resource):
-        super(DocumentChangeList, self).__init__(resource)
-        self.detect_sections()
-    
     @property
     def document(self):
         return self.resource.model
-    
-    def detect_sections(self):
-        return
     
     def get_paginator_kwargs(self):
         return {'per_page':self.resource.list_per_page,}
     
-    def get_links(self, state):
-        links = super(DocumentChangeList, self).get_links(state)
-        #links += self.getchangelist_sort_links(state)
+    def get_links(self):
+        links = super(DocumentChangeList, self).get_links()
+        #links += self.getchangelist_sort_links()
         return links
     
-    def get_changelist_sort_links(self, state):
+    def get_changelist_sort_links(self):
         links = list()
         return links
 
 class DotpathChangeList(ChangeList):
-    def __init__(self, resource):
-        super(DotpathChangeList, self).__init__(resource)
-        self.detect_sections()
-    
     @property
     def document(self):
         return self.resource.model
     
-    def populate_state(self, state):
+    def populate_state(self):
         pass
-    
-    def detect_sections(self):
-        return
     
     def get_paginator_kwargs(self):
         return {}
     
-    def get_links(self, state):
+    def get_links(self):
         return []
