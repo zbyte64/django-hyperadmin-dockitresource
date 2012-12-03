043fada523afa376fe2954f0cecfc31bfa09eb1e
Jason Kraus <zbyte64@gmail.com>
2012-12-03 13:17:16 -0800

    name update

--- dockitresource/endpoints.py
+++ dockitresource/endpoints.py
@@ -1,9 +1,8 @@
-from hyperadmin.resources.endpoints import EndpointLink, Endpoint
-from hyperadmin.resources.crud.endpoints import ListEndpoint, CreateEndpoint, DetailEndpoint, DeleteEndpoint, CreateEndpointLink, UpdateEndpointLink, DeleteEndpointLink
+from hyperadmin.resources.crud.endpoints import ListEndpoint, CreateEndpoint, DetailEndpoint, DeleteEndpoint, CreateLinkPrototype, UpdateLinkPrototype, DeleteLinkPrototype
 
 from dockit.schema.common import UnSet
 
-class DotpathCreateEndpointLink(CreateEndpointLink):
+class DotpathCreateLinkPrototype(CreateLinkPrototype):
     def handle_submission(self, link, submit_kwargs):
         form = link.get_form(**submit_kwargs)
         if form.is_valid():
@@ -12,7 +11,7 @@ class DotpathCreateEndpointLink(CreateEndpointLink):
             return self.on_success(resource_item)
         return link.clone(form=form)
 
-class DotpathUpdateEndpointLink(UpdateEndpointLink):
+class DotpathUpdateLinkPrototype(UpdateLinkPrototype):
     def handle_submission(self, link, submit_kwargs):
         form = link.get_form(**submit_kwargs)
         if form.is_valid():
@@ -22,7 +21,7 @@ class DotpathUpdateEndpointLink(UpdateEndpointLink):
             return self.on_success(resource_item)
         return link.clone(form=form)
 
-class DotpathDeleteEndpointLink(DeleteEndpointLink):
+class DotpathDeleteLinkPrototype(DeleteLinkPrototype):
     def handle_submission(self, link, submit_kwargs):
         instance = self.state.parent.instance
         instance.dot_notation_set_value(self.state.dotpath, UnSet)
@@ -33,7 +32,7 @@ class DotpathCreateEndpoint(DetailEndpoint):
     url_suffix = r'^(?P<dotpath>[\w\.]+)/add/$'
     
     def get_links(self):
-        return {'create':DotpathCreateEndpointLink(endpoint=self)}
+        return {'create':DotpathCreateLinkPrototype(endpoint=self)}
     
     def get_url(self):
         return super(CreateEndpoint, self).get_url(pk=self.state.parent.instance.pk, dotpath=self.state.dotpath)
@@ -42,9 +41,9 @@ class DotpathDetailEndpoint(DetailEndpoint):
     url_suffix = r'^(?P<dotpath>[\w\.]+)/$'
     
     def get_links(self):
-        return {'update':DotpathUpdateEndpointLink(endpoint=self),
-                'rest-update':DotpathUpdateEndpointLink(endpoint=self, link_kwargs={'method':'PUT'}),
-                'rest-delete':DotpathDeleteEndpointLink(endpoint=self, link_kwargs={'method':'DELETE'}),}
+        return {'update':DotpathUpdateLinkPrototype(endpoint=self),
+                'rest-update':DotpathUpdateLinkPrototype(endpoint=self, link_kwargs={'method':'PUT'}),
+                'rest-delete':DotpathDeleteLinkPrototype(endpoint=self, link_kwargs={'method':'DELETE'}),}
     
     def get_url(self, item):
         return super(DetailEndpoint, self).get_url(pk=item.instance.pk, dotpath=item.dotpath or self.state.dotpath)
@@ -53,7 +52,7 @@ class DotpathDeleteEndpoint(DeleteEndpoint):
     url_suffix = r'^(?P<dotpath>[\w\.]+)/delete/$'
     
     def get_links(self):
-        return {'delete':DotpathDeleteEndpointLink(endpoint=self)}
+        return {'delete':DotpathDeleteLinkPrototype(endpoint=self)}
     
     def get_url(self, item):
         return super(DeleteEndpoint, self).get_url(pk=item.instance.pk, dotpath=item.dotpath or self.state.dotpath)
